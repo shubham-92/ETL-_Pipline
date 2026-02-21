4fadbf23c17cb739ae79b7eb685aead908058282
shubham-92 <shubhamsj2933@gmail.com>
2026-02-21 18:27:53 +0530

    Refactor configuration loading and update requirements

--- config.py
+++ config.py
@@ -1,6 +1,15 @@
 import os
-from dotenv import load_dotenv
 
+# For local (.env)
+from dotenv import load_dotenv
 load_dotenv()
 
+# Try Streamlit secrets first, else fallback to .env
 DB_URL = os.getenv("DB_URL")
+
+try:
+    import streamlit as st
+    if "DB_URL" in st.secrets:
+        DB_URL = st.secrets["DB_URL"]
+except:
+    pass
